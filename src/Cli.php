0941b3040f19e32d0c094c6963e543ffbad99aad
Vladislav <trololo557@gmail.com>
2023-01-25 18:37:36 +0300

    greeting and configure aoutload

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hexlet\Cli;
+
+use function cli\prompt;
+use function cli\line;
+
+
+function greeting()
+{
+    line('Welcome to the Brain Game!');
+    $username = prompt('May I have your name?');
+    line("Hello, %s", $username);
+}
